3368afde6391228c6b918e0ad33593ebfd2d2e2e
anuprovo-debnath <166212030+anuprovo-debnath@users.noreply.github.com>
2026-02-01 13:15:47 +0000

    Deploying to gh-pages from @ anuprovo-debnath/anuprovo-debnath.github.io@fd519589a475ce861b03227c9aa4097c498b637e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "anuDev501/anuprovo_debnath",
+    "data-repo": "anuprovo-debnath/anuprovo-debnath.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
